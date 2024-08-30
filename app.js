0c1e64fb948d57bae07eda51bd6f5bc4fd627811
Danil gdj <danilguidjou7@gmail.com>
2024-08-30 01:43:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+app.listen(process.env.PORT_LISTEN);
