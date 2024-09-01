1024bfd5c027ea722d412f23509af747df94d83b
Danil gdj <danilguidjou7@gmail.com>
2024-09-01 22:55:39 +0100

    server

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ require("dotenv").config();
 var express = require("express");
 var app = express();
 
-app.get("/", function (req, res) {
-  res.send("hello world");
-});
+const indexRoutes = require("./routes/indexRoutes.js")
+const msgRoutes = require("./routes/msgRoutes.js")
+
+const path = require("node:path");
+
+//ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//css
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use("/", indexRoutes );
+
+// app.use("/message", msgRoutes)
 
 app.listen(process.env.PORT_LISTEN);
